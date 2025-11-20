f013ede8a1d8b8a1bc1ed603b49d6b968188f307
John Haroian <john.haroian@microchip.com>
2025-11-19 16:59:55 -0800

    Corrected path to linker file in dfp/

--- cmake/arm-none-eabi-gcc.cmake
+++ cmake/arm-none-eabi-gcc.cmake
@@ -6,4 +6,4 @@ set(CMAKE_C_COMPILER arm-none-eabi-gcc)
 
 set(CMAKE_C_FLAGS_INIT "-mcpu=cortex-m23 -ffunction-sections -fdata-sections --specs=nano.specs")
 
-set(CMAKE_EXE_LINKER_FLAGS_INIT "-Wl,--gc-sections -Wl,-T/home/c14029/Projects/blink-arm/dfp/PIC32CM-LS00/gcc/gcc/pic32cm5164ls00100_flash.ld")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-Wl,--gc-sections -Wl,-T/home/c14029/Projects/blink-arm-ls00/dfp/PIC32CM-LS00/gcc/gcc/pic32cm5164ls00100_flash.ld")
